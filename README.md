025442b6b4898298bf0301d57a2a757a8bf5fd88
Fonzie-byte <twistfons@live.nl>
2025-02-13 22:41:02 +0100

    [Feat] "Add player"-button!

--- README.md
+++ README.md
@@ -7,3 +7,6 @@ While it works for four players, I'd like to change the following:
 - [ ]  Add an "add player"-button, as well as maybe a "remove player" button
 - [ ]  Make the default bet adjustable, rather than  just 20
 - [ ]  Ideally, there should be something to make it more visible which players folded
+
+## Credit
+The background picture is from [https://www.photos-public-domain.com/2011/04/05/forest-green-canvas-fabric-texture/](https://www.photos-public-domain.com/2011/04/05/forest-green-canvas-fabric-texture/), and lies in the public domain.
--- functions.js
+++ functions.js
@@ -1,4 +1,26 @@
+let names = [
+	"Jennifer",
+	"Illiad",
+	"Helena",
+	"Geralt",
+	"Farida",
+	"Emile",
+	"Dahlia",
+	"Chris",
+	"Bob",
+	"Anna",
+]
+
 const total = document.getElementById("total")
+const playerArea = document.getElementById("player-area")
+
+function addPlayer(startingBet) {
+	const name = names.pop();
+	const id = name[0].toLowerCase()
+	// startingBet = Math.floor(Math.random() * 30) + 20
+
+	playerArea.innerHTML += '<tr><td><label for=' + id + '>' + name + '</label></td><td><input id=' + id + ' type=number value=' + startingBet + ' disabled></td><td><input class=current type=number min=0 value=0 onfocus="this.oldvalue = this.value" onchange="adjustValues(this, ' + id + ')"></td><td><button onclick="assignBet(' + id + ')">Won</button></td></tr>'
+}
 
 function adjustValues(sender, target) {
 	const difference = sender.value - sender.oldvalue
